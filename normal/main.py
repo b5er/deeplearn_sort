3b4510b5181802ab03d2a6151806c1da614bd0f7
b5er <bservia@gmail.com>
2019-01-15 15:17:20 -0500

    added normal sorting algorithms

--- normal/main.py
+++ normal/main.py
@@ -1,10 +1,61 @@
 from bubble_sort import BubbleSort
+from bucket_sort import BucketSort
+from heap_sort import HeapSort
+from merge_sort import MergeSort
+from quick_sort import QuickSort
 import time
 
+BUBBLE_SORT = True
+BUCKET_SORT = True
+HEAP_SORT = True
+MERGE_SORT = True
+QUICK_SORT = True
+
+
+
 if __name__ == '__main__':
-    b_sort = BubbleSort()
-    arr = [54, 45, 4, 3, 2, 1, 34, 35, 56, 7, 6, 76, 7, 67, 6, 7, 56, 5, 56, 6, 5, 65, 656, 56]
-    start = time.time()
-    b_sort.bubble_sort(arr)
-    end = time.time()
-    print(arr, f'time: {end - start} seconds')
+
+    arr = [12, 9, 20, 7, 5]
+
+    if BUBBLE_SORT:
+        bubb_sort = BubbleSort()
+        start = time.time()
+        bubb_sort.bubble_sort(arr)
+        end = time.time()
+        print('Bubble Sort::', arr, f'time: {end - start} seconds')
+
+    arr = [12, 9, 20, 7, 5]
+
+    if BUCKET_SORT:
+        buck_sort = BucketSort()
+        start = time.time()
+        buck_sort.bucket_sort(arr)
+        end = time.time()
+        print('Bucket Sort::', arr, f'time: {end - start} seconds')
+
+    arr = [12, 9, 20, 7, 5]
+
+    if HEAP_SORT:
+        h_sort = HeapSort()
+        start = time.time()
+        h_sort.heap_sort(arr)
+        end = time.time()
+        print('Heap Sort::', arr, f'time: {end - start} seconds')
+
+    arr = [12, 9, 20, 7, 5]
+
+    if MERGE_SORT:
+        m_sort = MergeSort()
+        start = time.time()
+        m_sort.merge_sort(arr)
+        end = time.time()
+        print('Merge Sort::', arr, f'time: {end - start} seconds')
+
+    arr = [12, 9, 20, 7, 5]
+
+    if QUICK_SORT:
+        q_sort = QuickSort()
+        start = time.time()
+        q_sort.quick_sort(0, len(arr) - 1, arr)
+        end = time.time()
+        print('Quick Sort::', arr, f'time: {end - start} seconds')
